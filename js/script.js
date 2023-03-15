2d5b87d232af1eb7af15ffe085730be311ac7090
DaneshShahpouri <danesh.shahpouri@gmail.com>
2023-03-15 13:26:15 +0100

    feat: milestone 1

--- js/script.js
+++ js/script.js
@@ -3,7 +3,47 @@ const { createApp } = Vue
 createApp({
   data() {
     return {
-      message: 'Hello Vue!'
+
+        todoList:[
+            {
+                text: 'Ricordati di fare una lista',
+                done: false,
+            },{
+                text: 'Ricordati di non fare una lista da un solo elemento',
+                done: false,
+            },{
+                text: 'Ricordati che le liste sono un insieme di elementi',
+                done: false,
+            },{
+                text: 'Ricordati che se la lista ha un solo elemento non può essere chiamata lista',
+                done: false,
+            },{
+                text: 'Ricordati che la lista, quando è fatta bene, è utile.',
+                done: false,
+            },
+        ],
+
+        newTodoElement: {
+            text: '',
+            done:false,
+        }
     }
-  }
+  },
+
+  methods:{
+
+    toggleDone(elementIndex) {
+
+        let donePara = this.todoList[elementIndex].done;
+
+        if(donePara === false){
+            this.todoList[elementIndex].done = true;
+        }else{
+            this.todoList[elementIndex].done = false;
+        }
+        
+        //console.log(donePara);
+    }
+
+  },
 }).mount('#app')
