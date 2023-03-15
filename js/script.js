9f739b19fb96bf58149ccb33858bd066f90da51a
DaneshShahpouri <danesh.shahpouri@gmail.com>
2023-03-15 15:30:33 +0100

    mileston 2

--- js/script.js
+++ js/script.js
@@ -7,26 +7,40 @@ createApp({
         todoList:[
             {
                 text: 'Ricordati di fare una lista',
+                modText: '',
                 done: false,
+                modVar: false,
             },{
                 text: 'Ricordati di non fare una lista da un solo elemento',
+                modText: '',
                 done: false,
+                modVar: false,
             },{
                 text: 'Ricordati che le liste sono un insieme di elementi',
+                modText: '',
                 done: false,
+                modVar: false,
             },{
                 text: 'Ricordati che se la lista ha un solo elemento non può essere chiamata lista',
+                modText: '',
                 done: false,
+                modVar: false,
             },{
                 text: 'Ricordati che la lista, quando è fatta bene, è utile.',
+                modText: '',
                 done: false,
+                modVar: false,
             },
         ],
 
         newTodoElement: {
             text: '',
+            modText: '',
             done:false,
-        }
+            modVar: false,
+        },
+
+        
     }
   },
 
@@ -43,7 +57,34 @@ createApp({
         }
         
         //console.log(donePara);
-    }
+    },
+
+    deleteElement(elementIndex){
+        this.todoList.splice( elementIndex, 1)
+    },
+
+    modifyElement(elementIndex){
+
+        
+        if(this.todoList[elementIndex].modVar===true){
+
+            this.todoList[elementIndex].text = this.todoList[elementIndex].modText
+            this.todoList[elementIndex].modVar=false;
+
+        }else{
+
+            let prevText = this.todoList[elementIndex].text;
+            //console.log(prevText)
+    
+            this.todoList[elementIndex].modText = prevText
+            //console.log(this.todoList[elementIndex].modText)
+    
+            this.todoList[elementIndex].text = ' . '
+
+            this.todoList[elementIndex].modVar=true;
+            
+        }        
+    },
 
   },
 }).mount('#app')
